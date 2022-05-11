9cb8712ebe131dc773c9e0b46eca5c6575b43da7
BenoitDossoine <benoit.dossoine@student.ehb.be>
2022-05-11 12:31:38 +0200

    Changed output format of featured games

--- index.js
+++ index.js
@@ -70,14 +70,16 @@ app.get('/game', async(req,res)=>{
 
 app.get('/featuredGames', async(req,res)=>{
     let featuredGamesIds = [1245620,1531540,860510,620];
-    let featuredGames = [];
+    let featuredGames = {
+        games: [],
+    };
     try{
         for(let id of featuredGamesIds){
             let url = `https://store.steampowered.com/api/appdetails?appids=${id}`
                 // fetch the data
                 let gameInfo = await fetch(url)
                     .then(response => response.json())
-                    .then(data=>featuredGames.push(data[`${id}`]["data"]));
+                    .then(data=>featuredGames.games.push(data[`${id}`]["data"]));
         }
     }catch(error){
         console.log(error);
